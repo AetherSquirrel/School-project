2672e8d840cdc182ec18d976383212d7ed4f4080
AetherSquirrel <33935823+AetherSquirrel@users.noreply.github.com>
2017-12-20 20:17:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ try:
         if users1[i][1]==user or users1[i][1]==user+'\n':  
             q.append(1)
 except:
-    None
-if len(q)==0: 
+    g=0
+if len(q)==0 and g!=0: 
     
     card=enterbox('Not registered.'+'\n'+"Input card: ")
     try: 
@@ -58,12 +58,12 @@ if len(q)==0:
     CARDS2.write(str((len(cards1))+1)+':: ')
     CARDS2.write(card+'\n')
     bal=str(0)
-elif len(q)>1:
+elif len(q)>1 and g!=0:
     
     card=enterbox('Oops... Do you have a clone?'+'\n'+"Input card to identificate: ")
     o=q[0]-1
     bal=balance[o][1]
-else:
+elif len(q)==1 and g!=0:
     o=q[0]-1
     card=cards1[o][1]
     bal=balance[o][1]
@@ -76,46 +76,49 @@ OPERATIONS.write('User: '+user+'\n')
 
 f=enterbox('What do you want to do?'+'\n'+'"Get" + summ to get money from your card.'+'\n'+'"Get all" to get all money from your card.'+'\n'+'"Put" + summ to put money on your card.'+'\n'+'"Balance" to know your balance.'+'\n'+'"Send" + card number + summ to send your money to someone'+'\n'+'"Exit" or "End" to finish work.')  
 
-f=f.lower()
-print(f)
-while f!='exit':
+try:
     f=f.lower()
-    s=f.split()
-    if s[0]=='put':
-        bal=str(int(bal)+int(s[1]))
-        f=enterbox('Done. Your balance now: '+bal)
-        
-    elif f=='get all':
-        bal='0'
-        f=enterbox('Done. Your balance now: '+bal)
-        
-    elif s[0]=='get':
-        if int(s[1])>int(bal):
-            f=enterbox('ERROR. Money limit: '+bal)
-        else:
-            bal=str(int(bal)-int(s[1]))
+    print(f)
+    while f!='exit':
+        f=f.lower()
+        s=f.split()
+        if s[0]=='put':
+            bal=str(int(bal)+int(s[1]))
             f=enterbox('Done. Your balance now: '+bal)
+        
+        elif f=='get all':
+            bal='0'
+            f=enterbox('Done. Your balance now: '+bal)
+        
+        elif s[0]=='get':
+            if int(s[1])>int(bal):
+                f=enterbox('ERROR. Money limit: '+bal)
+            else:
+                bal=str(int(bal)-int(s[1]))
+                f=enterbox('Done. Your balance now: '+bal)
             
-    elif s[0]=='send':
-        if int(s[2])>int(bal):
-                    f=enterbox('ERROR. Money limit: '+bal)        
-        if len(str(s[1]))!=8: 
-            f=enterbox('ERROR')
-        bal=str(int(bal)-int(s[2]))
-        for i in range(len(cards1)):
-            if cards1[i][1]==s[1]:
-                cashto=s[1]
-                cashnum=i
-                balance[i][1]+=s[2]
-                break
-        f=enterbox('Done. You have sent: '+s[2]+'\n'+'Your balance now:'+bal)    
+        elif s[0]=='send':
+            if int(s[2])>int(bal):
+                f=enterbox('ERROR. Money limit: '+bal)        
+            if len(str(s[1]))!=8: 
+                f=enterbox('ERROR')
+            bal=str(int(bal)-int(s[2]))
+            for i in range(len(cards1)):
+                if cards1[i][1]==s[1]:
+                    cashto=s[1]
+                    cashnum=i
+                    balance[i][1]+=s[2]
+                    break
+            f=enterbox('Done. You have sent: '+s[2]+'\n'+'Your balance now:'+bal)    
               
-    elif s[0]=='balance':
-        f=enterbox('Your balance now: '+bal)
-    else:
-        f=enterbox('Incorrect command.')
-    now=datetime.now()
-    OPERATIONS.write(f+' | '+(now.strftime("%d-%m-%Y %H:%M"))+'\n')
+        elif s[0]=='balance':
+            f=enterbox('Your balance now: '+bal)
+        else:
+            f=enterbox('Incorrect command.')
+            now=datetime.now()
+            OPERATIONS.write(f+' | '+(now.strftime("%d-%m-%Y %H:%M"))+'\n')
+except:
+    None
 try:
     balance[o][1]=str(int(bal))+'\n'
     for i in range(len(balance)):
